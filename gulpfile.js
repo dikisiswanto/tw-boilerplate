bf9a67ec8b60e892050c29be69251a26d867510b
Diki Siswanto <dickyachmady@gmail.com>
2021-01-02 11:57:02 +0800

    refactor(indentation): modify indentation from tabs to space

--- gulpfile.js
+++ gulpfile.js
@@ -10,48 +10,48 @@ const babel = require('gulp-babel');
 const include = require('gulp-include');
 
 gulp.task('css', () => {
-	const buildCss = gulp.src('./src/main.pcss')
-		.pipe(postcss())
-		.pipe(concat('style.css'))
-		.pipe(gulp.dest('./public/assets/css'))
-		.pipe(purgecss({
-			content: ['public/*.html']
-		}))
-		.pipe(prettyCss())
-		.pipe(concat('style.min.css'))
-		.pipe(gulp.dest('./public/assets/css/'));
-	return buildCss;
+  const buildCss = gulp.src('./src/main.pcss')
+    .pipe(postcss())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./public/assets/css'))
+    .pipe(purgecss({
+      content: ['public/*.html']
+    }))
+    .pipe(prettyCss())
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('./public/assets/css/'));
+  return buildCss;
 })
 
 gulp.task('html', () => {
-	const buildHtml = gulp.src('./src/pages/*.html')
-		.pipe(nunjucks({
-			path: ['./src/']
-		}))
-		.pipe(prettyHtml({
-			indent_size: 1,
-			indent_with_tabs: true
-		}))
-		.pipe(gulp.dest('./public/'));
-	return buildHtml
+  const buildHtml = gulp.src('./src/pages/*.html')
+    .pipe(nunjucks({
+      path: ['./src/']
+    }))
+    .pipe(prettyHtml({
+      indent_size: 1,
+      indent_with_tabs: true
+    }))
+    .pipe(gulp.dest('./public/'));
+  return buildHtml
 });
 
 gulp.task('js', () => {
-	const buildJs = gulp.src('./src/scripts/main.js')
-		.pipe(include())
-		.on('error', console.log)
-		.pipe(concat('script.js'))
-		.pipe(babel({
-			presets: ['@babel/preset-env']
-		}))
-		.pipe(gulp.dest('./public/assets/js/'))
-		.pipe(prettyJs())
-		.pipe(gulp.dest('./public/assets/js/'))
-	return buildJs;
+  const buildJs = gulp.src('./src/scripts/main.js')
+    .pipe(include())
+    .on('error', console.log)
+    .pipe(concat('script.js'))
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(gulp.dest('./public/assets/js/'))
+    .pipe(prettyJs())
+    .pipe(gulp.dest('./public/assets/js/'))
+  return buildJs;
 })
 
 gulp.task('watch', () => {
-	gulp.watch('./src/**/*.html', gulp.series('html'));
-	gulp.watch('./src/**/*.pcss', gulp.series('css'));
-	gulp.watch('./src/**/*.js', gulp.series('js'));
+  gulp.watch('./src/**/*.html', gulp.series('html'));
+  gulp.watch('./src/**/*.pcss', gulp.series('css'));
+  gulp.watch('./src/**/*.js', gulp.series('js'));
 })
